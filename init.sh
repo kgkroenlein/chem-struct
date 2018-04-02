2687d4d729e93817d1f4c0c9e968f90f15145dc5
Ken Kroenlein <kgkroenlein@gmail.com>
2018-04-02 13:59:11 -0600

    Corrected flag issue in init.sh

--- init.sh
+++ init.sh
@@ -8,4 +8,4 @@ docker image build -t rdkit-db -f Dockerfile-postgres .
 
 # Initialize database
 DOCKERDB=`docker run -d -p 54321:5432 --name chemstructdb rdkit-db`
-docker exec -it $DOCKERDB bash custom/init_data.sh
+docker exec $DOCKERDB bash custom/init_data.sh
