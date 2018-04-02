e8c50be61e596b3935f411d40f3ba8f488b04431
Ken Kroenlein <kgkroenlein@gmail.com>
2018-04-02 14:56:31 -0600

    Corrected role permission setting, x2

--- build-sql/table_creates.sql
+++ build-sql/table_creates.sql
@@ -1,17 +1,7 @@
 CREATE EXTENSION rdkit;
 CREATE SCHEMA emolecules;
 CREATE SCHEMA chembl;
-SELECT set_config(
-    'search_path',
-    'emolecules, ' || current_setting('search_path'),
-    false
-) WHERE current_setting('search_path') !~ '(^|\W)emolecules($|\W)';
-SELECT set_config(
-    'search_path',
-    'chembl, ' || current_setting('search_path'),
-    false
-) WHERE current_setting('search_path') !~ '(^|\W)chembl($|\W)';
-ALTER ROLE postgres SET search_path TO current_setting('search_path');
+ALTER ROLE postgres SET search_path TO emolecules, chembl, public;
 CREATE TABLE emolecules.raw_data (
     id SERIAL,
     smiles TEXT,
