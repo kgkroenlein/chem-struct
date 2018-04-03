45c8e7651449abf06c464c6d05d0d34671590681
Ken Kroenlein <kgkroenlein@gmail.com>
2018-04-03 12:22:24 -0600

    Init script bug

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ docker image build -t rdkit-app -f Dockerfile-rdkit .
 docker image build -t rdkit-db -f Dockerfile-postgres .
 
 # Initialize database
-DOCKERDB=`docker run -d -p 54321:5432 --name chemstructdb rdkit-db`
+DOCKERDB=`docker run -d -p 54321:5432 --name csdb rdkit-db`
 docker exec $DOCKERDB bash custom/init_data.sh
 
 docker run -d -p 8080:8080 --link csdb:db --name csapp rdkit-app python app/app.py
