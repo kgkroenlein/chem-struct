af8eeaedf527ad51ef041124ec3429fc8c70aea4
Ken Kroenlein <kgkroenlein@gmail.com>
2018-04-04 08:28:39 -0600

    Removed downloading data from init_data script out of respect

---
+++ fetch_data.sh
@@ -0,0 +1,16 @@
+#! bash
+cd "$(dirname "$0")"
+# if ! [ $(find "$search_dir" -name "$filename") ]; then
+#   echo "$filename is found in $search_dir"
+# else
+#   echo "$filename not found"
+# fi
+
+mkdir -p data
+
+apt-get update -y
+apt-get install curl -y
+
+curl http://downloads.emolecules.com/free/2018-03-01/version.smi.gz > data/version.smi.gz
+curl ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/latest/chembl_23_postgresql.tar.gz > data/chembl_23_postgresql.tar.gz
+curl https://pubs.acs.org/doi/suppl/10.1021/ci034243x/suppl_file/ci034243xsi20040112_053635.txt > data/ESOL.csv
--- init_data.sh
+++ init_data.sh
@@ -1,23 +1,18 @@
 #!bash
 cd "$(dirname "$0")"
 
-apt-get update -y
-apt-get install curl -y
-curl http://downloads.emolecules.com/free/2018-03-01/version.smi.gz > version.smi.gz
-curl ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/latest/chembl_23_postgresql.tar.gz > chembl_23_postgresql.tar.gz
-
 createdb -U postgres chemstruct
 psql -U postgres -f build-sql/table_creates.sql -d chemstruct
-zcat version.smi.gz                            \
+zcat data/version.smi.gz                       \
 | sed '1d; s/\\/\\\\/g'                        \
 | psql  -U postgres -d chemstruct -c           \
 "COPY raw_data (smiles, emol_id, parent_id) FROM stdin WITH DELIMITER ' '"
-tar -xzf chembl_23_postgresql.tar.gz
+tar -xzf data/chembl_23_postgresql.tar.gz
 PATH_CACHE=`psql -U postgres -tc 'SHOW search_path'`
 psql -U postgres -d chemstruct -c              \
 "ALTER ROLE postgres SET search_path TO chembl;"
 pg_restore --no-owner -U postgres -d chemstruct \
-    chembl_23/chembl_23_postgresql/chembl_23_postgresql.dmp
+    data/chembl_23/chembl_23_postgresql/chembl_23_postgresql.dmp
 psql -U postgres -d chemstruct -c              \
 "ALTER ROLE postgres SET search_path TO $PATH_CACHE;"
 psql -U postgres -f build-sql/feature_gen.sql -d chemstruct
