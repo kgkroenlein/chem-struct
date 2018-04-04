3114a0e6c61efe21670bc69cbc32d7b3b5b3adcb
Ken Kroenlein <kgkroenlein@gmail.com>
2018-04-04 13:45:37 -0600

    Added ESOL b/c ACS changes link

--- fetch_data.sh
+++ fetch_data.sh
@@ -13,4 +13,6 @@ apt-get install curl -y
 
 curl http://downloads.emolecules.com/free/2018-03-01/version.smi.gz > data/version.smi.gz
 curl ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/latest/chembl_23_postgresql.tar.gz > data/chembl_23_postgresql.tar.gz
-curl https://pubs.acs.org/doi/suppl/10.1021/ci034243x/suppl_file/ci034243xsi20040112_053635.txt > data/ESOL.csv
+# ACS moves the link for no apparent reason
+# Just cache the file for the sake of simplicity
+#curl https://pubs.acs.org/doi/suppl/10.1021/ci034243x/suppl_file/ci034243xsi20040112_053635.txt > data/ESOL.csv
