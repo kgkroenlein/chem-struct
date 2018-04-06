82c70bb4ad83876b89afd6ef0ba2415140e5e19d
Ken Kroenlein <kgkroenlein@gmail.com>
2018-04-06 12:18:01 -0600

    bug fix

--- scripts/rf.py
+++ scripts/rf.py
@@ -54,4 +54,4 @@ if __name__ == '__main__':
         X_train, X_test, y_train, y_test = \
             train_test_split(X, y, test_size=0.2, random_state=42)
         model = train_rf(X_train, y_train)
-        print("{}:\t{.4f}".format(fp,model.score(X_test, y_test)))
+        print("{}:\t{:.4f}".format(fp,model.score(X_test, y_test)))
