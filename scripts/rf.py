99324b772cbca2d4afe3d30786ec141778956362
Ken Kroenlein <kgkroenlein@gmail.com>
2018-04-08 14:51:01 -0600

    Updated randomforest model

--- scripts/rf.py
+++ scripts/rf.py
@@ -1,20 +1,23 @@
-import psycopg2
-import os
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 import numpy as np
 from rdkit import DataStructs
 
-'''
-Initialize singleton global variables: database connection
-'''
-conn = psycopg2.connect(database='chemstruct',
-                        user='postgres',
-                        host=os.environ['DB_PORT_5432_TCP_ADDR'],
-                        port=os.environ['DB_PORT_5432_TCP_PORT'],
-                        )
 
-def pull_data(fp):
+def aws_context_db():
+    '''
+    Connect to the standard deployment database
+    '''
+    conn = psycopg2.connect(database='chemstruct',
+                            user='postgres',
+                            host=os.environ['DB_PORT_5432_TCP_ADDR'],
+                            port=os.environ['DB_PORT_5432_TCP_PORT'],
+                            )
+    return conn
+
+def pull_data(fp, conn = None):
+    import psycopg2
+    import os
     '''
     Pull all fingerprints and the associated label from the database for
     lipophilicity table
@@ -22,6 +25,9 @@ def pull_data(fp):
     Input: fp {str}  Which fingerprint to pull:
             'mfp2', 'ffp2', 'torsionbv', 'atompair', 'rdkitbv', 'maccs'
     '''
+
+    if not conn:
+        conn = aws_context_db()
 
     sql = '''
     SELECT  rdk.fps.{} AS fp,
@@ -38,8 +44,25 @@ def pull_data(fp):
     y = []
 
     for row in cur:
-        X += [DataStructs.cDataStructs.CreateFromBitString(row[0])]
+        X += [fp2bits(row[0])]
         y += [row[1]]
+
+    return np.array(X), np.array(y)
+
+def fp2bits(fp):
+    ebv = DataStructs.cDataStructs.CreateFromBitString(fp)
+    stash = np.zeros((1,))
+    DataStructs.ConvertToNumpyArray(ebv, stash)
+    return stash
+
+def restore_data(fp, filename='fps.pkl'):
+    import pickle
+    with open(filename, 'rb') as f:
+        data = pickle.load(f)
+
+    ids = [i for i in data] # Guarentee traversal order
+    X = [fp2bits(data[id][fp]) for id in ids]
+    y = [data[id]['value'] for id in ids]
 
     return np.array(X), np.array(y)
 
@@ -50,7 +73,8 @@ def train_rf(X, y):
 
 if __name__ == '__main__':
     for fp in ('mfp2', 'ffp2', 'torsionbv', 'atompair', 'rdkitbv', 'maccs'):
-        X, y = pull_data(fp)
+        # X, y = pull_data(fp)
+        X, y = restore_data(fp)
         X_train, X_test, y_train, y_test = \
             train_test_split(X, y, test_size=0.2, random_state=42)
         model = train_rf(X_train, y_train)
